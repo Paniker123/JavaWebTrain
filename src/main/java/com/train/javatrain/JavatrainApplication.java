45f0b939b9991c84cc65410249567aa2f29ff282
VitaliiSuslin <shakrek@gmail.com>
2019-01-08 18:22:37 +0200

    Initial commit

---
+++ src/main/java/com/train/javatrain/JavatrainApplication.java
@@ -0,0 +1,14 @@
+package com.train.javatrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavatrainApplication.class, args);
+    }
+
+}
+
